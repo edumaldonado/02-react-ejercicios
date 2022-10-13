bda9e7eba1867ea543625d09842c5c82936bdaeb
edumaldonado <edumaldonado@gmail.com>
2022-10-13 18:58:28 -0300

    fin día trabajo 2022-10-13

---
+++ src/component/CrudApp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const initialDB = [
+    {
+        id : 1,
+        name : "Seya",
+        constellation : "Pegaso"
+    },    {
+        id : 2,
+        name : "Shiryu",
+        constellation : "Dragón"
+    },
+    {
+        id : 3,
+        name : "Hyoga",
+        constellation : "Cisne"
+    },
+    {
+        id : 4,
+        name : "Shun",
+        constellation : "Andrómeda"
+    },
+]
+
+const CrudApp =()=>{
+    return(
+        <>
+            <h1> CRUD APP</h1>
+            <form></form>
+        </>
+    )
+}
+
+export default CrudApp;
