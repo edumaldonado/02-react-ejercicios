d05b10e67bab85800a0b59f956563b664ec7a533
edumaldonado <edumaldonado@gmail.com>
2022-10-21 19:14:11 -0300

    fin de dia trabajo 2022-10-21

--- src/component/CrudApp.js
+++ src/component/CrudApp.js
@@ -1,63 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import CrudForm from './CrudForm';
-import CrudTable from './CrudTable';
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
 
 const initialDB = [
-    {
-        id : 1,
-        name : "Seya",
-        constellation : "Pegaso"
-    },    {
-        id : 2,
-        name : "Shiryu",
-        constellation : "Dragón"
-    },
-    {
-        id : 3,
-        name : "Hyoga",
-        constellation : "Cisne"
-    },
-    {
-        id : 4,
-        name : "Shun",
-        constellation : "Andrómeda"
-    },
-]
+  {
+    id: 1,
+    name: "Seya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragón",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andrómeda",
+  },
+];
 
+const CrudApp = () => {
+  const [db, setDb] = useState(initialDB);
+  const [dataToEdit, setDataToEdit] = useState(null);
 
+  const createData = (data) => {
+    data.id = Date.now(); //para tratar de crear un id unico
+    setDb([...db, data]);
+  };
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `Esta seguro de eliminar el registro con el id  ${id}`
+    );
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    }
+    //else{
 
-const CrudApp =()=>{
-    const [db, setDb] = useState(initialDB);
-    const [dataToEdit, setDataToEdit] = useState(null);
-
-    const createData = (data) =>{
-        data.id = Date.now(); //para tratar de crear un id unico
-        setDb([...db,data]);
-    };
-    const updateData = (data) =>{
-        let newData = db.map(el => el.id === data.id? data : el );
-        setDb(newData);
-    };
-    const deleteData = (id) =>{
-        let isDelete = window.confirm(`Esta seguro de eliminar el registro con el id  ${id}`);
-        if (isDelete){
-            let newData = db.filter(el => el.id !== id);
-            setDb(newData);
-        }
-        //else{
-
-        //}
-    };
-    return(
-        <>
-            <h2> CRUD App</h2>
-            <article className="grid-1-2">
-            <CrudForm createData = {createData} updateData ={updateData} dataToEdit={dataToEdit} setDataToEdit={setDataToEdit}/>
-            <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData ={deleteData} />
-            </article>
-        </>
-    )
-}
+    //}
+  };
+  return (
+    <>
+      <h2> CRUD App</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          data={db}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
+    </>
+  );
+};
 
 export default CrudApp;
--- src/component/CrudTable.js
+++ src/component/CrudTable.js
@@ -1,25 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import CrudTableRow from './CrudTableRow';
+import React, { useState, useEffect } from "react";
+import CrudTableRow from "./CrudTableRow";
 
-const CrudTable = ({data,setDataToEdit, deleteData}) => {
+const CrudTable = ({ data, setDataToEdit, deleteData }) => {
+  return (
+    <div>
+      <h3>Tabla de Datos</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Constelación</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length === 0 ? (
+            <tr>
+              <td colSpan="3">Sin Datos</td>
+            </tr>
+          ) : (
+            data.map((el) => (
+              <CrudTableRow
+                key={el.id}
+                el={el}
+                setDataToEdit={setDataToEdit}
+                deleteData={deleteData}
+              />
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-    return ( 
-        <div>
-            <h3>Tabla de Datos</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Constelación</th>
-                        <th>Acciones</th>   
-                    </tr>
-                </thead>
-                <tbody>
-                {data.length === 0? <tr><td colSpan="3">Sin Datos</td></tr>:  data.map((el) => <CrudTableRow key={el.id} el={el} setDataToEdit ={setDataToEdit} deleteData ={deleteData} />)}
-                </tbody>
-            </table>
-        </div>
-     );
-}
- 
 export default CrudTable;
